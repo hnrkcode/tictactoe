756c9971fc118e01b57836726723a3de09dce544
Henrik Petersson <henrik@tutamail.com>
2020-05-16 14:16:33 +0200

    First working implementaion

--- src/js/index.js
+++ src/js/index.js
@@ -1,97 +1,152 @@
-class Player {
-  constructor(name, active, color) {
-    this.name = name;
-    this.active = active;
-    this.color = color;
+const grid = document.querySelector(".grid");
+const activePlayer = document.getElementById("active-player");
+const cells = [];
+const size = 9;
+let cellsLeft = size;
+
+const setActivePlayer = player => {
+  activePlayer.textContent = player;
+};
+
+const getActivePlayer = () => {
+  return activePlayer.textContent;
+};
+
+const switchActivePlayer = () => {
+  if (getActivePlayer() === "Player 1") {
+    setActivePlayer("Player 2");
+  } else {
+    setActivePlayer("Player 1");
   }
+};
 
-  changeStatus() {
-    this.active = !this.active;
-  }
-}
+const renderGrid = () => {
+  // Initialize active player.
+  setActivePlayer("Player 1");
 
-// Creates the tic tac tow board.
-class Board {
-  constructor() {
-    this._render();
-  }
-
-  // Render a grid with nine cells.
-  _render() {
-    const grid = document.getElementById("tictactoe");
-    const cells = 9;
-
-    for (let i = 0; i < cells; i++) {
-      const cell = document.createElement("div");
-      grid.append(cell);
-    }
-
-    this._hoverHighlight(grid);
-  }
-
-  // Highlight the active cell.
-  _hoverHighlight(elem) {
-    // Mouse moves into cell.
-    elem.addEventListener("mouseover", event => {
-      if (!event.target.id && event.target.style.backgroundColor === "") {
-        event.target.style.backgroundColor = "red";
-      }
-    });
-
-    // Mouse moves out from cell.
-    elem.addEventListener("mouseout", event => {
-      if (!event.target.id && event.target.style.backgroundColor == "red") {
-        event.target.style.backgroundColor = "";
-      }
+  // Render grid.
+  for (let i = 0; i < size; i++) {
+    const cell = document.createElement("div");
+    grid.append(cell);
+    cells.push({
+      "element": cell,
+      "belongsTo": null
     });
   }
-}
+};
 
-class TicTacToe {
-  constructor() {
-    this.board = new Board();
-    this.playerOne = new Player("Player 1", true, "blue");
-    this.playerTwo = new Player("Player 2", false, "purple");
+const resetGame = () => {
+  grid.innerHTML = "";
+  cells.length = 0;
+  cellsLeft = size;
+  renderGrid();
+};
+
+const toggleHoverEffect = (event) => {
+  const classList = event.target.classList;
+  const activePlayer = getActivePlayer();
+
+  for (let cell of cells) {
+    // Only toggle the hover effect class on cells that isn't already taken.
+    if (cell.element === event.target && cell.belongsTo === null && !classList.contains("grid")) {
+      activePlayer === "Player 1" ? classList.toggle("player1-hover") : classList.toggle("player2-hover");
+    }
+  }
+};
+
+// Convert a 1D array to a 2D array.
+const convertArray = (arr1D) => {
+  const arr2D = []
+
+  for (let i = 0, j = -1; i < size; i++) {
+    if (i % 3 === 0) {
+      j++;
+      arr2D[j] = [];
+    }
+    arr2D[j].push(arr1D[i]);
   }
 
-  startGame() {
-    const grid = document.getElementById("tictactoe");
+  return arr2D;
+};
 
-    grid.addEventListener("click", event => {
-      if (!event.target.id) {
-        if (this.playerOne.active) {
-          this._updateBoard(this.playerOne);
-        } else if (this.playerTwo.active) {
-          this._updateBoard(this.playerTwo);
-        }
-      }
-    });
-  }
+const isVictory = () => {
+  const array2D = convertArray(cells);
+  for (let i = 0; i < 3; i++) {
+    // Horizontal line.
+    if (array2D[i][0].belongsTo === "Player 1" && array2D[i][1].belongsTo === "Player 1" && array2D[i][2].belongsTo === "Player 1") {
+      return true;
+    } else if (array2D[i][0].belongsTo === "Player 2" && array2D[i][1].belongsTo === "Player 2" && array2D[i][2].belongsTo === "Player 2") {
+      return true;
+    }
 
-  _updateBoard(player) {
-    const activePlayer = document.querySelector("h2");
-
-    if (this._isEmptyCell(event)) {
-      activePlayer.innerText = `Active player: ${player.name}`;
-      event.target.style.backgroundColor = player.color;
-      this._changeStatus();
+    // Vertical line.
+    if (array2D[0][i].belongsTo === "Player 1" && array2D[1][i].belongsTo === "Player 1" && array2D[2][i].belongsTo === "Player 1") {
+      return true;
+    } else if (array2D[0][i].belongsTo === "Player 2" && array2D[1][i].belongsTo === "Player 2" && array2D[2][i].belongsTo === "Player 2") {
+      return true;
     }
   }
 
-  _changeStatus() {
-    this.playerOne.changeStatus();
-    this.playerTwo.changeStatus();
-    console.log(this.playerOne.active, this.playerTwo.active);
-  }
-
-  _isEmptyCell(event) {
-    if (event.target.style.backgroundColor !== "red") {
-      return false;
-    }
-
+  // Diagonal lines.
+  if (array2D[0][0].belongsTo === "Player 1" && array2D[1][1].belongsTo === "Player 1" && array2D[2][2].belongsTo === "Player 1") {
+    return true;
+  } else if (array2D[0][0].belongsTo === "Player 2" && array2D[1][1].belongsTo === "Player 2" && array2D[2][2].belongsTo === "Player 2") {
+    return true;
+  } else if (array2D[0][2].belongsTo === "Player 1" && array2D[1][1].belongsTo === "Player 1" && array2D[2][0].belongsTo === "Player 1") {
+    return true;
+  } else if (array2D[0][2].belongsTo === "Player 2" && array2D[1][1].belongsTo === "Player 2" && array2D[2][0].belongsTo === "Player 2") {
     return true;
   }
+
+  return false;
+};
+
+const isDraw = () => {
+  if (cellsLeft === 0) {
+    return true;
+  }
+
+  return false;
 }
 
-const tictactoe = new TicTacToe();
-tictactoe.startGame();
+const clickedCell = (event) => {
+  const classList = event.target.classList;
+
+  // Cell now belongs to the player that clicked on it.
+  if (!classList.contains("player1-clicked") || !classList.contains("player2-clicked")) {
+    getActivePlayer() === "Player 1" ? classList.add("player1-clicked") : classList.add("player2-clicked");
+    classList.remove("player1-hover", "player2-hover");
+    cellsLeft--;
+    switchActivePlayer();
+  }
+
+  // Check if the player has won.
+  if (isVictory()) {
+    resetGame();
+  }
+
+  // Check if it's a draw.
+  if (isDraw()) {
+    resetGame();
+  }
+};
+
+grid.addEventListener("mouseover", event => {
+  toggleHoverEffect(event);
+});
+
+grid.addEventListener("mouseout", event => {
+  toggleHoverEffect(event);
+});
+
+grid.addEventListener("click", event => {
+  cells.forEach(cell => {
+    // Do this if the clicked cell isn't taken.
+    if (cell.belongsTo === null && cell.element === event.target) {
+      cell.belongsTo = getActivePlayer();
+      clickedCell(event);
+    }
+  });
+});
+
+renderGrid();
